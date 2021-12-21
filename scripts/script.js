8ee420ed45c61e5d6ec123f2a3ea16c3837ecdaf
Jesse <jesse.earley@gmail.com>
2021-12-21 08:10:15 -0500

    added an event listener for DOMContentLoaded. Added code to output the copyright information in the footer with the current year.

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,27 @@
-const sliders = document.querySelectorAll(".slideIn");
-const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -200px 0px"
-};
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const sliders = document.querySelectorAll(".slideIn");
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -200px 0px"
+    };
 
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach( entry => {
-        if (!entry.isIntersecting){
-            return;
-        }
-        else{
-            entry.target.classList.add('slid');
-            appearOnScroll.unobserve(entry.target);
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach( entry => {
+            if (!entry.isIntersecting){
+                return;
+            }
+            else{
+                entry.target.classList.add('slid');
+                appearOnScroll.unobserve(entry.target);
 
-        }
+            }
+        })
+    }, appearOptions);
+
+    sliders.forEach(slider =>{
+        appearOnScroll.observe(slider);
     })
-}, appearOptions);
 
-sliders.forEach(slider =>{
-    appearOnScroll.observe(slider);
-})
+    let date =  new Date().getFullYear();
+    document.getElementById('FooterText').innerHTML = "Copyright &copy; " + date + " JesseEarley.com. All Rights Reserved";
+});
